1044282499b6c4a33ad66e58f49e68cef57fab52
SalathielGenese <salathielgenese@gmail.com>
2019-03-22 14:21:46 +0100

    setup parse script

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const { xsltProcess: process, xmlParse: parse } = require( 'xslt-processor' );
+const { promises: fs } = require( 'fs' );
+
+
+( async () =>
+{
+    const html = process(
+        parse( await getContent( `${ __dirname }/test.xml` ) ),
+        parse( await getContent( `${ __dirname }/test.xsl` ) ),
+    );
+
+    console.log( html );
+})();
+
+
+async function getContent( path )
+{
+    return ( await fs.readFile( path ) ).toString();
+}
